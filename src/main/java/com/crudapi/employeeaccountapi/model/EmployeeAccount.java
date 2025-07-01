c9d701935f6c0b48f4451980fef07b3dcf47cd53
Khushinet <unnatiagarwal121@gmail.com>
2025-07-01 18:40:47 +0530

    Initial Commit

--- src/main/java/com/crudapi/employeeaccountapi/model/EmployeeAccount.java
+++ src/main/java/com/crudapi/employeeaccountapi/model/EmployeeAccount.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 public class EmployeeAccount {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String bankAccountNumber;
